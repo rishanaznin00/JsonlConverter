71fd67056459109169a489fbeb1d13f5350c82cb
rishanaznin00 <rishanaznin@gmail.com>
2022-10-22 12:51:26 +0600

    Update JsonlConverter.java

--- jsonl-converter/src/main/java/com/jsonl/JsonlConverter.java
+++ jsonl-converter/src/main/java/com/jsonl/JsonlConverter.java
@@ -29,98 +29,118 @@ import java.util.stream.Stream;
 import org.apache.commons.lang3.math.NumberUtils;
 
 public class JsonlConverter {
-	
-	
+	private static final int SUCCESS = 1;
+	private static final int FAILED = 0;
 
 	public static void main(String[] args) {
-		//String os = System.getProperty("os.name").substring(0, 7);
+		// String os = System.getProperty("os.name").substring(0, 7);
 		Scanner s = new Scanner(System.in);
 		System.out.println("please specify the input file: ");
 		String inputFileName = s.nextLine();
 		System.out.println("please enter the delimeter: ");
-		final String delimeter=s.nextLine();
+		final String delimeter = s.nextLine();
 		System.out.println("please enter the skip charecter: ");
-		final String skipCharacter=s.nextLine();
+		final String skipCharacter = s.nextLine();
 		s.close();
-		generateJsonlFile(inputFileName, delimeter, skipCharacter);
-		
+
+		String splitChar = "(?<!" + Pattern.quote(skipCharacter) + ")" + Pattern.quote(delimeter);
+		int nameStrIndx = inputFileName.lastIndexOf("\\");
+		String outputFileName = inputFileName.substring(0, nameStrIndx + 1)
+				+ inputFileName.substring(nameStrIndx + 1, inputFileName.length()) + "-jsonl-converted";
+
+		int result = generateJsonlFile(inputFileName, delimeter, splitChar, outputFileName);
+		if (result == SUCCESS)
+			System.out.println("Jsonl formated file generated successfully in " + outputFileName);
+		else
+			System.out.println("Some error occured! Please try again....");
+
 	}
 
-
-	private static void generateJsonlFile(String inputFileName, final String delimeter, String skipCharacter) {
+	private static int generateJsonlFile(String inputFileName, final String delimeter, String splitChar,
+			String outputFileName) {
 		Path inputPath = Paths.get(inputFileName);
 		try {
+			//Creating the output file
+			File outputFile = new File(outputFileName);
+			outputFile.createNewFile();
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(outputFile));
+			
+			//Getting the field values of json format
 			String firstLine = Files.lines(inputPath).findFirst().get();
-
 			final List<String> fields = Arrays.asList(firstLine.split(Pattern.quote(delimeter)));
-		
+			
+			String secondLine = Files.lines(inputPath).skip(1).limit(2).findFirst().get();
 			try (Stream<String> lines = Files.lines(inputPath)) {
-				int nameStrIndx = inputFileName.lastIndexOf("\\");
-				File outputFile = new File(inputFileName.substring(0, nameStrIndx + 1)
-						+ inputFileName.substring(nameStrIndx + 1, inputFileName.length()) + "-jsonl-converted");
-				outputFile.createNewFile();
+				//writing the 1st line with no comma at the end.
+				try {
+					outputStreamWriter.append("[{");
+					String[] values = secondLine.split(splitChar);
+					StringBuilder sb = new StringBuilder();
+					for (int i = 0; i < fields.size() && i < values.length; i++) {
+						String val = formatValue(values[i]);
+						sb.append("\"" + fields.get(i) + "\":" + val + (i < fields.size() - 1 ? "," : ""));
+					}
+					outputStreamWriter.append(sb + "}");
+				} catch (IOException e) {
+					throw e;
+				}
 
-				OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(outputFile));
-				outputStreamWriter.append("[");
-				lines.skip(1).forEach(line -> {
+				//writing the rest of lines with comma at the start
+				lines.skip(2).forEach(line -> {
 					try {
-
-						outputStreamWriter.append("{");
+						outputStreamWriter.append(",\n" + "{");
 						System.out.println(line);
-						String[] values = line.split("(?<!"+Pattern.quote(skipCharacter)+")"+Pattern.quote(delimeter));
-					//	String[] values = line.split(Pattern.quote(delimeter));
+						String[] values = line.split(splitChar);
 						StringBuilder sb = new StringBuilder();
-						for (int i = 0; i < fields.size() && i <values.length; i++) {
-							String val=formatValue( values[i]);
-							sb.append("\""+fields.get(i)+ "\":" + val+ (i< fields.size()-1?",":""));
+						for (int i = 0; i < fields.size() && i < values.length; i++) {
+							String val = formatValue(values[i]);
+							sb.append("\"" + fields.get(i) + "\":" + val + (i < fields.size() - 1 ? "," : ""));
 						}
-						System.out.println(sb);
-						outputStreamWriter.append(sb + "},"+"\n");
+						outputStreamWriter.append(sb + "}");
 
 					} catch (IOException e) {
-						
-						e.printStackTrace();
+						throw new RuntimeException();
 					}
 				});
 				outputStreamWriter.append("]");
 				outputStreamWriter.close();
-				// System.out.println(firstLine);
-				
 
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				throw e;
+			}finally {
+				outputStreamWriter.close();
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return FAILED;
 		}
+
+		return SUCCESS;
 	}
 
-	
 	public static String formatDate(String value) {
-		if(value.trim().length()!=10) return value;
-		DateTimeFormatter requiredFormat=  DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		List<String> dateFormats= Arrays.asList("dd/MM/yyyy", "yyyy/MM/dd", "dd-MM-yyyy", "yyyy-MM-dd");
-	    for(String format:dateFormats) {
-	    	    DateTimeFormatter dtf= DateTimeFormatter.ofPattern(format);
-	    		try {
-					LocalDate  parsedDate=LocalDate.parse(value, dtf);
-					value=parsedDate.format(requiredFormat);
-					break;
-				} catch (DateTimeParseException e) {
-					
-				}
-			
-	    }
-	   
+		if (value.trim().length() != 10)
+			return value;
+		DateTimeFormatter requiredFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		List<String> dateFormats = Arrays.asList("dd/MM/yyyy", "yyyy/MM/dd", "dd-MM-yyyy", "yyyy-MM-dd");
+		for (String format : dateFormats) {
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
+			try {
+				LocalDate parsedDate = LocalDate.parse(value, dtf);
+				value = parsedDate.format(requiredFormat);
+				break;
+			} catch (DateTimeParseException e) {
+
+			}
+		}
 		return value;
 	}
+
 	public static String formatValue(String value) {
-		if(NumberUtils.isParsable(value)) return value;
-		value= "\""+formatDate(value)+"\"";
-		
+		if (NumberUtils.isParsable(value))
+			return value;
+		value = "\"" + formatDate(value) + "\"";
+
 		return value;
 	}
-	
+
 }
